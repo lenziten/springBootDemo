f4d9f7825493f00cb59adb16e96b05516349b6a9
lenziten <lenziten@hotmail.com>
2018-06-24 17:44:16 +0800

    add log4j

--- src/main/java/com/web/CloseController.java
+++ src/main/java/com/web/CloseController.java
@@ -1,14 +1,20 @@
 package com.web;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.domain.User;
@@ -18,6 +24,8 @@ import com.service.UserService;
 @Controller
 @RequestMapping("/exit")
 public class CloseController {
+	
+	private Logger log = LoggerFactory.getLogger(CloseController.class);
 	
     @Autowired
     private ApplicationContext appContext;
@@ -38,12 +46,14 @@ public class CloseController {
     	return "forward:/index.jsp";
     }
     
-    @RequestMapping(value="updateById",method=RequestMethod.GET)
-    public String updateById(){
-    	User user = new User();
-    	user.setUserId("1B20D00E-6C8B-42B1-88B6-DD8577FD0720");
-    	Integer result = userService.updateById(user);
-    	return result.toString();
+
+    @ResponseBody
+    @RequestMapping(value="getDate",method=RequestMethod.GET)
+    public Date getDate() {
+    	Date result = userService.getDate();
+    	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss:ssss");
+    	log.debug("debugging msg : now is " + sdf.format(result));
+    	return result;
     }
     
     @RequestMapping("/shutDown")
